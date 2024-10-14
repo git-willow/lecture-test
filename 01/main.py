16979038846c5f4fd3176e9732e5c83d55350de6
root <root@DESKTOP-TVGENNU.localdomain>
2024-10-14 15:37:45 +0900

    fifth commit

--- 01/main.py
+++ 01/main.py
@@ -3,5 +3,5 @@ from utils.count import count_word
 if __name__ == "__main__":
     s = "hello world"
     print("e:", count_word(s, 'e'))
-    print("o:", count_word(s, 'e'))
-    print("l:", count_word(s, 'e'))
+    print("o:", count_word(s, 'o'))
+    print("l:", count_word(s, 'l'))
