cdc34a2b0ac39e8649c486de9e3d04c669879367
root <root@DESKTOP-TVGENNU.localdomain>
2024-11-01 16:21:18 +0900

    second commit

--- 02/numpy/main.py
+++ 02/numpy/main.py
@@ -9,7 +9,7 @@ print("Data shape:", data.shape)
 print("各クラスの科目別平均点:\n", np.mean(data, axis = 1))
 print("全クラスの番号3番の学生の中で2科目目の最高得点:", np.max(data[:,2,1]))
 print("各科目で一番点数が高い人と低い人の点差:", np.max(data.reshape(15, -1), axis = 0) - np.min(data.reshape(15, -1), axis = 0))
-print("各クラスで1科目目が80点を超えている人数:", np.sum(data[:,:,0] > 80, axis = 1))
+print("各クラスで1科目目が80点以上の人数:", np.sum(data[:,:,0] >= 80, axis = 1))
 print("2科目の合計点が135点を超えている人数:", np.sum(np.sum(data, axis = 2) > 135))
 print("全生徒の1科目目と2科目目の相関係数:",
         np.sum((data[:,:,0] - np.mean(data[:,:,0])) * (data[:,:,1] - np.mean(data[:,:,1]))) / 
